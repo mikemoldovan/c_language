8b0c5fc7c7ba19a2036d2b8dc59abf0078bd23a3
mikemoldovan <mika.moldovan@gmail.com>
2015-05-03 18:52:38 +0300

    Update needle.c

--- needle.c
+++ needle.c
@@ -93,12 +93,12 @@ int main() {
 	int gap_pnlt, mism_pnlt, mtch_rev, len1, len2;
 	char seqfile1[30], seqfile2[30], str1[50000], str2[50000];
 	
-	printf("Gap penalty: "); 			scanf("%d", &gap_pnlt);
+	printf("Gap penalty: ");      scanf("%d", &gap_pnlt);
 	printf("Mismatch penalty: "); scanf("%d", &mism_pnlt);
-	printf("Match reward: "); 		scanf("%d", &mtch_rev);
+	printf("Match reward: ");     scanf("%d", &mtch_rev);
 	printf("Input files containing plain sequences\n")
 	printf("First sequence: ");   scanf("%s", seqfile1);
-	printf("Second sequence: ");	scanf("%s", seqfile2);
+	printf("Second sequence: ");  scanf("%s", seqfile2);
 	
 	FILE *seq1 = fopen(seqfile1, "r");
 	FILE *seq2 = fopen(seqfile2, "r");
